fc7e816a637a51fa506adaea159c059906b8bfd0
moises <1128moises@gmail.com>
2024-12-17 19:25:50 -0400

    feat: update portfolio metadata with personal branding and enhanced description

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,9 @@ const lato = Lato({
 });
 
 export const metadata = {
-  title: "Portafolio",
-  description: "This is my Portafolio",
+  title: "Portafolio Moises Perez",
+  description:
+    "Dedicated Frontend Developer with 3+ years of experience an with a passion for crafting intuitive and visually appealing user experiences. Proficient in Tailwind, Javascript, Next.js, React, React Native, Expo Go, BitBucket ,Github, Notion, Figma, Postman, Axios, TypeScript, FireBase, Api integration, HTML, CSS, JavaScript. Committed to staying up-to-date with the latest web technologies and design trends. Aspiring to elevate my skills and contribute to innovative projects as a Senior Developer.",
 };
 
 export default function RootLayout({ children }) {
